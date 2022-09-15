550ca30218fa549d4e965d7e013821534e6e0c6f
Rustamjon Tolipov <rustamxontolipov788@gmail.com>
2022-09-15 11:47:05 +0500

    Update parser.rb

--- app/sources/parser.rb
+++ app/sources/parser.rb
@@ -1,13 +1,12 @@
 class ReusableNokogiri
-  BASE_URL = 'https://'
+  BASE_URL = 'https://'.freeze
   DATE_REGEX = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/
 
   # Formats
-  IMG_SRC = 'src'
-  DATETIME = 'datetime'
-  HREF = 'href'
-  HTTP = 'http'
-  
+  IMG_SRC = 'src'.freeze
+  DATETIME = 'datetime'.freeze
+  HREF = 'href'.freeze
+  HTTP = 'http'.freeze
 
   def initialize(news_item, title, link, image, text)
     @news_item = news_item
@@ -17,37 +16,41 @@ class ReusableNokogiri
     @text = text
   end
 
+  def title(doc)
+    doc.css(@title).text
+  end
+
+  def image(doc)
+    doc.css(@image).attr(IMG_SRC)
+  end
+
+  def text(doc)
+    doc.css(@text).text
+  end
+
+  def link(doc)
+    doc.css(@link).attr(HREF)
+  end
+
+  def source(doc)
+    doc.css(@image).attr(IMG_SRC)
+  end
+
+  def parser(doc)
+    {
+      title: title(doc),
+      url: link(doc),
+      image_url: image(doc),
+      published_at: Time.now,
+      description: text(doc),
+      source: source(doc)
+    }
+  end
+
   def parse
-    def title(doc)
-      doc.css(@title).text
-    end
-
-    def image(doc)
-      doc.css(@image).attr(IMG_SRC)
-    end
-
-    def text(doc)
-      doc.css(@text).text
-    end
-
-    def link(doc)
-      doc.css(@link).attr(HREF)
-    end
-
-    def source(doc)
-      doc.css(@image).attr(IMG_SRC)
-    end
-
     doc = Nokogiri::HTML(URI.open(BASE_URL + @url))
     doc.css(@news_item).map do |product|
-      {
-        title: title(product),
-        url: link(product),
-        image_url: image(product),
-        published_at: Time.now, 
-        description: text(product),
-        source: source(product)
-      }
+      parser(product)
     end
   end
 end
